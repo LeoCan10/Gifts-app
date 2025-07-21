c9bc4a60f8b5cdbc5d6e814e4d9438b9494b9d7d
Leonardo Cangialosi <leocangialosi04@gmail.com>
2025-07-21 03:22:32 -0300

    side-menu changes, icons & more

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,9 +1,35 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption{
+  label: string;
+  subLabel: string;
+  icon: string;
+  route: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOption[]=[
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: 'dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: 'dashboard/search',
+    }
+  ]
+
+
+}
